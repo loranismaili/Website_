d2d9828e740a82083dd635c5df29b50711083234
loranismaili <loranismaili1@gmail.com>
2025-04-27 17:38:59 +0100

    toggle effect

--- contact.js
+++ contact.js
@@ -4,3 +4,18 @@ $('#btn3').click(function(){
 $('#btn4').click(function(){
     $('.hidden').hide();
 });
+
+
+const toggleButton = document.getElementById('btn-toggle');
+const body = document.body;
+
+toggleButton.addEventListener('click', function() {
+    
+    body.classList.toggle('coffe-theme');
+   
+    if (body.classList.contains('coffe-theme')) {
+        toggleButton.textContent = 'Classic Theme';
+    } else {
+        toggleButton.textContent = 'Coffe Theme';
+    }
+});

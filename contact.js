1a6cf552ccfb8ae0905a8cad47d7a1b8c95f4898
loranismaili <loranismaili1@gmail.com>
2025-04-27 18:39:28 +0100

    .

--- contact.js
+++ contact.js
@@ -1,3 +1,24 @@
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    console.log("Name: " + name);
+    console.log("Email: " + email);
+    console.log("Message: " + message);
+
+    if (name === '' || email === '' || message === '') {
+        alert('Please fill in all fields.');
+    } else {
+        alert('Thank you for your message!');
+    }
+});
+
+
+
 $('#btn3').click(function(){
     $('.hidden').show('fast');
 });
